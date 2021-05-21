4491ea8d84cd92f17121bdd4eb18e1bc77a60c44
Eric Lamphere <ericjlamphere@gmail.com>
2021-05-21 18:32:53 -0400

    modifying some util pipe code

--- R/utils.R
+++ R/utils.R
@@ -2,43 +2,59 @@
 #' @description Search for matches to an argument pattern within each element of a character vector. Uses perl style regex and ignores case. See \code{grepl} documentation for more information.
 #' @param x Character vector
 #' @param reg Character string containing a regular expression to be matched in the given character vector.
-#' @return See \code{grepl} documentation for more information.
+#' @return Logical, whether or not `x` matches `reg.` See \code{grepl} documentation for more information.
 #' @export
 `%~%` <- function(x, reg){
-  grepl(reg, x, ignore.case = TRUE, perl = TRUE)
+  res <- grepl(reg, x, ignore.case = TRUE, perl = TRUE)
+  return(res)
 }
 
 #' Anti-Pattern Matching
 #' @description Search for anti-matches to an argument pattern within each element of a character vector. Uses perl style regex and ignores case. See \code{grepl} documentation for more information.
 #' @param x Character vector
 #' @param reg Character string containing a regular expression to be anti-matched in the given character vector.
-#' @return See \code{grepl} documentation for more information.
+#' @return Logical, whether or not `x` doesn't match `reg.` See \code{grepl} documentation for more information.
 #' @export
 `%not~%` <- function(x, reg){
-  !grepl(reg, x, ignore.case = TRUE, perl = TRUE)
+  res <- !grepl(reg, x, ignore.case = TRUE, perl = TRUE)
+  return(res)
 }
 
 #' Concatenate Strings
 #' @description Concatenate vectors after converting to character and ignoring NAs.
 #' @param x First R object to be converted to a character vector and concatenated to the left of \code{y}.
 #' @param y Second R object to be converted to a character vector and concatenated to the right of \code{x}.
-#' @return See \code{paste} documentation for more information.
+#' @return Character, concatenated strings `x` and `y`, less NAs. See \code{paste} documentation for more information.
 #' @export
 `%&%` <- function(x, y){
-  paste0(
+  # only print messages if length(x) == 1, otherwise the message is too long
+  if(length(x) == 1 && length(y) == 1) {
+    nas <- c(x = is.na(x), y = is.na(y))
+
+    if (all(nas)) {
+      cli::cli_alert("Both x and y are NA, returning empty string")
+    } else if (any(nas)) {
+      cli::cli_alert("{names(nas[nas])} is NA, returning {names(nas[!nas])}")
+    }
+  }
+
+  res <- paste0(
     ifelse(is.na(x),"",x),
     ifelse(is.na(y),"",y)
   )
+
+  return(res)
 }
 
 #' Anti-Value Matching
 #' @description Match returns a vector of the positions of (first) anti-matches of its first argument in its second.
 #' @param x Vector or NULL: the values to be matched. Long vectors are supported.
 #' @param y Vector or NULL: the values to be matched against. Long vectors are not supported.
-#' @return See \code{%in%} documentation for more information.
+#' @return Logical, whether or not `x` was in `y`. See \code{%in%} documentation for more information.
 #' @export
 `%notin%` <- function(x, y){
-  !(x %in% y)
+  res <- !(x %in% y)
+  return(res)
 }
 
 
@@ -46,10 +62,11 @@
 #' @description \code{gsub} with \code{ignore.case = TRUE, perl = TRUE, fixed = FALSE}.
 #' @param x Character vector.
 #' @param pattern Character string containing a regular expression to be matched and removed from \code{x}.
-#' @return See \code{%in%} documentation for more information.
+#' @return Character, `x` with `pattern` removed
 #' @export
-`%remove%` <- function(x, pattern){
-  gsub(pattern, "", x, ignore.case = TRUE, perl = TRUE, fixed = FALSE)
+`%del%` <- function(x, pattern){
+  res <- gsub(pattern, "", x, ignore.case = TRUE, perl = TRUE, fixed = FALSE)
+  return(res)
 }
 
 
